a07579af60756bf982e62895be197cd403e8cca3
ELCBAh <6211@holbertonstudents.com>
2023-10-01 15:45:09 +0000

    added exec permissions to every .js file again

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     let amenitees = {};
-  
+
     $('input[type="checkbox"]').change(function () {
       let id = $(this).data('id');
       let name = $(this).data('name');
-  
+
       if ($(this).is(':checked')) {
         amenitees[id] = name;
       } else {
         delete amenitees[id];
       }
-  
+
       let amenities = Object.values(amenitees).join(', ');
       if (amenities.length > 30) {
         amenities = amenities.substring(0, 30) + '...';
@@ -19,7 +19,7 @@ $(document).ready(function () {
       $('.amenities h4').html(amenities);
     });
   });
-  
+
   $.ajax({
     url: "http://localhost:5001/api/v1/status/",
     type: "GET",
